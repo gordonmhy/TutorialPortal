b0e1aea296dc988c1aa3f16352dcd13a88fa1ed7
gordonmhy <gh@hpri.33mail.com>
2021-08-03 01:58:49 +0800

    Panel GUI changes works on more pages

--- tutorialportal/static/panel-toggles.js
+++ tutorialportal/static/panel-toggles.js
@@ -1,11 +1,15 @@
 function studentManagerPanels() {
-    const panels = ["Credentials", "Attendance", "Payment"]
-    panels.forEach((button) => {
-        $(`.${button}Button`).on('shown.bs.collapse', () => {
-            panels.forEach((toClose) => {
-                if (toClose !== button) {
-                    $(`.${toClose}Button`).collapse('hide')
-                }
+    const specifiedPanels = ['Credentials', 'Attendance', 'Payment']
+    const mainPanels = ['SelectStudent', 'AddStudent']
+    const panelGroups = [specifiedPanels, mainPanels]
+    panelGroups.forEach((panels) => {
+        panels.forEach((button) => {
+            $(`.${button}Button`).on('shown.bs.collapse', () => {
+                panels.forEach((toClose) => {
+                    if (toClose !== button) {
+                        $(`.${toClose}Button`).collapse('hide')
+                    }
+                })
             })
         })
     })
